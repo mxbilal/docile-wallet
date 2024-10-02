41e89cd3133d1e8c99cd4f503abb7057caad30b6
bilal <mxbilal0@gmail.com>
2024-10-02 08:13:28 +0500

    fix

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use("/user", userRoute);
 app.use("/payment", paymentRoute);
 
 app.listen(3004, "0.0.0.0", () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3004");
 });
